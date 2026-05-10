c7b23324ec6ad97616cf5f110b4d05c7e1729825
Joshua Barnett <joshua@barne.tt>
2026-05-10 20:39:37 +0000

    refactor build targets

--- joust.mk
+++ joust.mk
@@ -1,6 +1,5 @@
-JOUST_ROM_URL := https://archive.org/download/ArcadeMachinesChampionCollection2/Champion%20Collection%20-%20Arcade%20%28H-L%29.zip/joust.zip
-JOUST_ZIP_FILE := joust.zip
-JOUST_ROM_DIRECTORY ?= roms
+BIN_DIRECTORY := bin
+JOUST_ROM_DIRECTORY ?= $(BIN_DIRECTORY)/roms
 JOUST_ROM_FILES := \
 3006-13.1b \
 3006-14.2b \
@@ -41,7 +40,6 @@ joust_rom_9b_3006-21.a6 \
 joust_rom_10b_3006-22.a7 \
 joust_rom_11b_3006-23.c7 \
 joust_rom_12b_3006-24.e7
-JOUST_REWRITE_SOUND_ROM_FILE := video_sound_rom_4_std_780.ic12xxx
 JOUST_BYTE_OFFSETS := \
 0x0000-0x0FFF \
 0x1000-0x1FFF \
@@ -56,6 +54,16 @@ JOUST_BYTE_OFFSETS := \
 0xE000-0xEFFF \
 0xF000-0xFFFF
 JOUST_REWRITE_ROM_FILES := $(addprefix $(JOUST_ROM_DIRECTORY)/,$(JOUST_REWRITE_ROM_FILES))
+JOUST_REWRITE_SOUND_ROM_FILE := $(JOUST_ROM_DIRECTORY)/video_sound_rom_4_std_780.ic12
+JOUST_REWRITE_DECODER_ROM_FILES := \
+$(JOUST_ROM_DIRECTORY)/decoder_rom_4.3g \
+$(JOUST_ROM_DIRECTORY)/decoder_rom_6.3c
+JOUST_REWRITE_ZIP := $(JOUST_ROM_DIRECTORY)/joust.zip
+JOUST_REWRITE_FILES := \
+$(JOUST_REWRITE_ROM_FILES) \
+$(JOUST_REWRITE_SOUND_ROM_FILE) \
+$(JOUST_REWRITE_DECODER_ROM_FILES)
+JOUST_REWRITE_ROM_PAIRS := $(join $(addsuffix |,$(JOUST_REWRITE_ROM_FILES)),$(JOUST_BYTE_OFFSETS))
 ZIP_EXTENSION := zip
 BIN_EXTENSION := bin
 JOUST := joust
@@ -65,9 +73,7 @@ JOUST_VERSIONS := $(JOUST) $(JOUSTR) $(JOUSTY)
 JOUST_PATHS := $(addprefix $(JOUST_ROM_DIRECTORY)/,$(JOUST_VERSIONS))
 JOUST_ZIPS := $(addsuffix .$(ZIP_EXTENSION),$(JOUST_PATHS))
 JOUST_BINS := $(addsuffix .$(BIN_EXTENSION),$(JOUST_PATHS))
-ASM6809 := asm6809
 CACHE_DIRECTORY := cache
-BIN_DIRECTORY := bin
 SRC_ORIGINAL_DIRECTORY := src/original
 SRC_REWRITE_DIRECTORY := src/rewrite
 SRC_EXTENSION := SRC
@@ -76,6 +82,21 @@ LST_EXTENSION := LST
 SRCS := $(wildcard $(SRC_ORIGINAL_DIRECTORY)/*.$(SRC_EXTENSION))
 OBJS := $(patsubst $(SRC_ORIGINAL_DIRECTORY)/%.$(SRC_EXTENSION),$(BIN_DIRECTORY)/%.$(OBJ_EXTENSION),$(SRCS))
 LSTS := $(patsubst $(SRC_ORIGINAL_DIRECTORY)/%.$(SRC_EXTENSION),$(BIN_DIRECTORY)/%.$(LST_EXTENSION),$(SRCS))
+JOUST_REWRITE_PROGRAM_SOURCES := \
+$(SRC_REWRITE_DIRECTORY)/make.ASM \
+$(SRC_REWRITE_DIRECTORY)/JOUSTI.ASM \
+$(SRC_REWRITE_DIRECTORY)/TB12REV3.ASM \
+$(SRC_REWRITE_DIRECTORY)/RAMDEF.ASM \
+$(SRC_REWRITE_DIRECTORY)/EQU.ASM \
+$(SRC_REWRITE_DIRECTORY)/MESSEQU.ASM \
+$(SRC_REWRITE_DIRECTORY)/MESSEQU2.ASM \
+$(SRC_REWRITE_DIRECTORY)/MESSAGE.ASM \
+$(SRC_REWRITE_DIRECTORY)/PHRASE.ASM \
+$(SRC_REWRITE_DIRECTORY)/ATT.ASM \
+$(SRC_REWRITE_DIRECTORY)/SYSTEM.ASM \
+$(SRC_REWRITE_DIRECTORY)/JOUSTRV4.ASM \
+$(SRC_REWRITE_DIRECTORY)/T12REV3.ASM \
+$(SRC_REWRITE_DIRECTORY)/joust_mods.ASM
 
 .ONESHELL:
 
@@ -94,42 +115,54 @@ LSTS := $(patsubst $(SRC_ORIGINAL_DIRECTORY)/%.$(SRC_EXTENSION),$(BIN_DIRECTORY)
 # $(JOUST_REWRITE_ROM_FILES): $(JOUST_ROM_DIRECTORY) $(BIN_DIRECTORY)/joust.p
 # 	p2bin $(BIN_DIRECTORY)/joust.p $@ -l 00 -r $(word $(call pos,$@,$(JOUST_REWRITE_ROM_FILES)),$(JOUST_BYTE_OFFSETS))
 
-_pos = $(if $(findstring $1,$2),$(call _pos,$1,$(wordlist 2,$(words $2),$2),x $3),$3)
-pos = $(words $(call _pos,$1,$2))
+.PHONY: build clean
+build: $(JOUST_REWRITE_ZIP)
 
-.PHONY: build
-build:
-	rm -rf bin
-	mkdir -p bin
-	asl src/rewrite/make.ASM -L -olist bin/joust.lst -o bin/joust.p
-	rm -rf roms
-	mkdir -p roms
-	p2bin bin/joust.p roms/joust_rom_1b_3006-13.e4 -l 00 -r 0x0000-0x0FFF
-	p2bin bin/joust.p roms/joust_rom_2b_3006-14.c4 -l 00 -r 0x1000-0x1FFF
-	p2bin bin/joust.p roms/joust_rom_3b_3006-15.a4 -l 00 -r 0x2000-0x2FFF
-	p2bin bin/joust.p roms/joust_rom_4b_3006-16.e5 -l 00 -r 0x3000-0x3FFF
-	p2bin bin/joust.p roms/joust_rom_5b_3006-17.c5 -l 00 -r 0x4000-0x4FFF
-	p2bin bin/joust.p roms/joust_rom_6b_3006-18.a5 -l 00 -r 0x5000-0x5FFF
-	p2bin bin/joust.p roms/joust_rom_7b_3006-19.e6 -l 00 -r 0x6000-0x6FFF
-	p2bin bin/joust.p roms/joust_rom_8b_3006-20.c6 -l 00 -r 0x7000-0x7FFF
-	p2bin bin/joust.p roms/joust_rom_9b_3006-21.a6 -l 00 -r 0x8000-0x8FFF
-	p2bin bin/joust.p roms/joust_rom_10b_3006-22.a7 -l 00 -r 0xD000-0xDFFF
-	p2bin bin/joust.p roms/joust_rom_11b_3006-23.c7 -l 00 -r 0xE000-0xEFFF
-	p2bin bin/joust.p roms/joust_rom_12b_3006-24.e7 -l 00 -r 0xF000-0xFFFF
-	asl src/rewrite/VSNDRM4.ASM -L -olist bin/vsndrm4.lst -o bin/vsndrm4.p
-	p2bin bin/vsndrm4.p roms/video_sound_rom_4_std_780.ic12 -l 00 -r 0xF000-0xFFFF
-	asl src/rewrite/decoder_roms.asm -L -olist bin/decoder.lst -o bin/decoder.p
-	p2bin bin/decoder.p roms/decoder_rom_4.3g -r 0x0000-0x01FF
-	p2bin bin/decoder.p roms/decoder_rom_6.3c -r 0x0200-0x03FF
-	zip roms/joust.zip roms/*
+clean:
+	rm -rf $(BIN_DIRECTORY) $(JOUST_ROM_DIRECTORY)
+
+$(BIN_DIRECTORY) $(JOUST_ROM_DIRECTORY):
+	mkdir -p $@
+
+$(BIN_DIRECTORY)/joust.p $(BIN_DIRECTORY)/joust.lst: $(JOUST_REWRITE_PROGRAM_SOURCES) | $(BIN_DIRECTORY)
+	asl $(SRC_REWRITE_DIRECTORY)/make.ASM -L -olist $(BIN_DIRECTORY)/joust.lst -o $(BIN_DIRECTORY)/joust.p
+
+define JOUST_REWRITE_ROM_RULE
+$(word 1,$(subst |, ,$(1))): $(BIN_DIRECTORY)/joust.p | $(JOUST_ROM_DIRECTORY)
+	p2bin $$< $$@ -l 00 -r $(word 2,$(subst |, ,$(1)))
+endef
+
+$(foreach rom_pair,$(JOUST_REWRITE_ROM_PAIRS),$(eval $(call JOUST_REWRITE_ROM_RULE,$(rom_pair))))
+
+$(BIN_DIRECTORY)/vsndrm4.p $(BIN_DIRECTORY)/vsndrm4.lst: $(SRC_REWRITE_DIRECTORY)/VSNDRM4.ASM | $(BIN_DIRECTORY)
+	asl $< -L -olist $(BIN_DIRECTORY)/vsndrm4.lst -o $(BIN_DIRECTORY)/vsndrm4.p
+
+$(JOUST_REWRITE_SOUND_ROM_FILE): $(BIN_DIRECTORY)/vsndrm4.p | $(JOUST_ROM_DIRECTORY)
+	p2bin $< $@ -l 00 -r 0xF000-0xFFFF
+
+$(BIN_DIRECTORY)/decoder.p $(BIN_DIRECTORY)/decoder.lst: $(SRC_REWRITE_DIRECTORY)/decoder_roms.asm | $(BIN_DIRECTORY)
+	asl $< -L -olist $(BIN_DIRECTORY)/decoder.lst -o $(BIN_DIRECTORY)/decoder.p
+
+$(JOUST_ROM_DIRECTORY)/decoder_rom_4.3g: $(BIN_DIRECTORY)/decoder.p | $(JOUST_ROM_DIRECTORY)
+	p2bin $< $@ -r 0x0000-0x01FF
+
+$(JOUST_ROM_DIRECTORY)/decoder_rom_6.3c: $(BIN_DIRECTORY)/decoder.p | $(JOUST_ROM_DIRECTORY)
+	p2bin $< $@ -r 0x0200-0x03FF
+
+$(JOUST_REWRITE_ZIP): $(JOUST_REWRITE_FILES) | $(JOUST_ROM_DIRECTORY)
+	rm -f $@
+	zip $@ $^
 
 # .PHONY: rewrite
 # rewrite: $(JOUST_REWRITE_ROM_FILES)
 
+# ASM6809 := asm6809
 # $(BIN_DIRECTORY)/%.$(LST_EXTENSION) $(BIN_DIRECTORY)/%.$(OBJ_EXTENSION): $(SRC_ORIGINAL_DIRECTORY)/%.$(SRC_EXTENSION)
 # 	mkdir -p $(@D)
 # 	$(ASM6809) $< --output=$@ --listing=$(@:.$(OBJ_EXTENSION)=.$(LST_EXTENSION)) || true
 
+# JOUST_ROM_URL := https://archive.org/download/ArcadeMachinesChampionCollection2/Champion%20Collection%20-%20Arcade%20%28H-L%29.zip/joust.zip
+# JOUST_ZIP_FILE := joust.zip
 # $(JOUST_ROM_FILES):
 # 	curl \
 # 	--silent \
